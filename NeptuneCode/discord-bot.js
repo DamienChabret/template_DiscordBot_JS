8a18e6de899b72d4167857763e068f1c4c1c7732
Damien CHABRET <damien.chabret@iut-dijon.u-bourgogne.fr>
2023-02-02 00:02:33 +0100

    Clean Code

--- NeptuneCode/discord-bot.js
+++ NeptuneCode/discord-bot.js
@@ -1,3 +1,4 @@
+// ---------------- VARIABLES
 // Récupère les variables dans les bon fichiers 
 const { CLIENT_TOKEN } = require('./config.json')
 const { Client, GatewayIntentBits } = require('discord.js');
@@ -5,9 +6,20 @@ const { Client, GatewayIntentBits } = require('discord.js');
 // Instancie client
 const client = new Client({intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages]});
 
-// Quand le bot est prêt  
-client.on('ready', () => {
-   console.log(`Logged in as ${client.user.tag}!`);
-})
-client.login(CLIENT_TOKEN); // Connecte le bot avec le secret tokken
-// https://discordjs.guide/creating-your-bot/#using-config-json
+
+// ------------------ PROGRAMME PRINCIPAL
+clientLogin();
+
+
+
+// ----------------- METHODES 
+/**
+ * Quand le client est prêt
+ * // https://discordjs.guide/creating-your-bot/#using-config-json
+ */
+function clientLogin(){
+   client.on('ready', () => {
+      console.log(`Logged in as ${client.user.tag}!`);
+   })
+   client.login(CLIENT_TOKEN); // Connecte le bot avec le secret tokken
+}
